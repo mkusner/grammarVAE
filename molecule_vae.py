2472deb80baf04c1be25bea3d7548dff1dbf9f62
simranjit <kohlisimranjit@gmail.com>
2019-01-15 23:50:20 -0500

    added comments

--- molecule_vae.py
+++ molecule_vae.py
@@ -1,11 +1,11 @@
 import nltk
 import numpy as np
-from six.moves import xrange
-import re
 import zinc_grammar
 import models.model_zinc
 import models.model_zinc_str
-
+# added the imports below
+from six.moves import xrange
+import re
 
 def get_zinc_tokenizer(cfg):
     long_tokens = filter(lambda a: len(a) > 1, cfg._lexical_index.keys())
@@ -73,6 +73,8 @@ class ZincGrammarModel(object):
         """ Encode a list of smiles strings into the latent space """
         assert type(smiles) == list
         tokens = map(self._tokenize, smiles)
+        # parse_trees = [self._parser.parse(t).next() for t in tokens]
+        # Above commented line gives error method not found 'next()' hence updated it to below line
         parse_trees = [self._parser.parse(t).__next__() for t in tokens]
         productions_seq = [tree.productions() for tree in parse_trees]
         indices = [np.array([self._prod_map[prod] for prod in entry], dtype=int) for entry in productions_seq]
@@ -109,6 +111,8 @@ class ZincGrammarModel(object):
                           self._productions[i].rhs()) 
                    for i in sampled_output]
             for ix in xrange(S.shape[0]):
+                # S[ix].extend(map(str, rhs[ix])[::-1])
+                # Above commented line gives error can't index map like that hence updated it to below line
                 S[ix].extend(list(map(str, rhs[ix]))[::-1])
         return X_hat # , ln_p
 
